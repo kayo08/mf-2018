ff103d6c339dda29faa85da96b371e53c7b53e3d
kayo08 <k8martins08@gmail.com>
2018-09-10 20:16:24 -0300

    Atividade Dia da Semana

---
+++ aula-3/semana/br/ufg/inf/mf/pratica/Aplicacao.java
@@ -0,0 +1,10 @@
+package br.ufg.inf.mf.pratica;
+
+import java.time.LocalDate;
+
+public class Aplicacao {
+    public static void main (String[] args){
+        System.out.println(LocalDate.now().getDayOfWeek());
+    }
+
+}
